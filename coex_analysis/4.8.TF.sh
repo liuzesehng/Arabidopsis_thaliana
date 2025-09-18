e118580f9acf7e998e11f9aa6531948b9ed98331
liuzesehng <liuzesheng@nenu.edu.cn>
2025-09-18 10:15:19 +0800

    coex_modication

--- coex_analysis/4.8.TF.sh
+++ coex_analysis/4.8.TF.sh
@@ -2,9 +2,9 @@
 
 # 提取all.fimo.out.filt的第一列
 cut -f1 all.fimo.out.filt > all.fimo_col1.txt
-awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt total/coex-brown-salmon.node.csv > total/gene.TF_ids.txt
-awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt a/a.coex-yellow.node.csv > a/gene.TF_ids.txt
-awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt b/b.coex-blue-pink.node.csv > b/gene.TF_ids.txt
+awk 'NR==FNR {a[$1]; next} $9 in a' all.fimo_col1.txt all.gene_id.txt > total/gene.TF_ids.txt
+awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt a/a.gene_id.txt > a/gene.TF_ids.txt
+awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt b/b.gene_id.txt > b/gene.TF_ids.txt
 
 # 在第一行前添加标题行
 sed -i '1iname\tDegree' total/gene.TF_ids.txt
