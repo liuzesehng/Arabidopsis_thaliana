b1648f14563949d67ee24b485c27bec2982f3316
liuzesehng <liuzesheng@nenu.edu.cn>
2025-09-18 11:38:22 +0800

    TF_modication

--- coex_analysis/4.8.TF.sh
+++ coex_analysis/4.8.TF.sh
@@ -2,7 +2,7 @@
 
 # 提取all.fimo.out.filt的第一列
 cut -f1 all.fimo.out.filt > all.fimo_col1.txt
-awk 'NR==FNR {a[$1]; next} $9 in a' all.fimo_col1.txt all.gene_id.txt > total/gene.TF_ids.txt
+awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt total/all.gene_id.txt > total/gene.TF_ids.txt
 awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt a/a.gene_id.txt > a/gene.TF_ids.txt
 awk 'NR==FNR {a[$1]; next} $1 in a' all.fimo_col1.txt b/b.gene_id.txt > b/gene.TF_ids.txt
 
